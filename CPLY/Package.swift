c7a738588488b5454a33d5b0910c77909b6488e7
Eren <erndmrcn42@gmail.com>
2025-12-30 21:37:22 +0300

    extra folder removed

--- CPLY/Package.swift
+++ CPLY/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .target(
             name: "CPLY",
-            path: "CPLY",
+            path: ".",
             sources: [
                 "wrapper.cpp",
                 "miniply.cpp"
